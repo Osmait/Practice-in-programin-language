20e78a322ba5de0e1a34844204b2100f10ea0395
Saul Burgos <saulburgos6@gmail.com>
2023-06-17 22:59:37 -0400

    request  in rust

--- nodejs/index.ts
+++ nodejs/index.ts
@@ -1,34 +1,20 @@
 import fs from "fs/promises";
-import puppeteer from "puppeteer";
 
-// fetch("https://dummyjson.com/products/1")
-//   .then((r) => r.json())
-//   .then((r) => fs.writeFileSync("product.json", JSON.stringify(r)));
-
-// interface Product {
-//   id: number;
-//   title: string;
-//   description: string;
-//   price: number;
-// }
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+}
 
 async function Main() {
-  //   const response = await fetch("https://dummyjson.com/products/1");
-  //   const data = await response.json();
-  //   //   console.log(data);
-  //   const result = await fs.readFile("product.json", "utf8");
-  //   const product: Product = JSON.parse(result);
-  //   console.log(product);
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
-  await page.goto(
-    "https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood"
-  );
+  await fetch("https://dummyjson.com/products/1")
+    .then((r) => r.json())
+    .then((r) => fs.writeFile("product.json", JSON.stringify(r)));
 
-  const title = await page.waitForSelector("h1 > .title-name");
-  console.log(title);
-
-  await browser.close();
+  const result = await fs.readFile("product.json", "utf8");
+  const product: Product = JSON.parse(result);
+  console.log(product);
 }
 Main();
 
