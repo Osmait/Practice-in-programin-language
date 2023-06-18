83053118bce8af94987843c76144d1c40302d0e1
Saul Burgos <saulburgos6@gmail.com>
2023-06-18 00:08:02 -0400

    add kotlin implement

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -1,7 +1,49 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+import kotlinx.serialization.json.Json
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+
+
+
+
+fun main(args: Array<String>) {
+    val apiUrl = "https://dummyjson.com/products/1"
+
+    // Hacer la petición GET
+    val response = getRequest(apiUrl)
+//    println(response)
+    val result = Json.decodeFromString<Product>(response)
+
+
+    // Imprimir la respuesta
+    println(result.description)
+}
+
+
+fun getRequest(url: String): String {
+    val apiUrl = URL(url)
+    val connection = apiUrl.openConnection() as HttpURLConnection
+    connection.requestMethod = "GET"
+
+    val responseCode = connection.responseCode
+    val responseBody = StringBuilder()
+
+    if (responseCode == HttpURLConnection.HTTP_OK) {
+        val reader = BufferedReader(InputStreamReader(connection.inputStream))
+        var line: String? = reader.readLine()
+        while (line != null) {
+            responseBody.append(line)
+            line = reader.readLine()
+        }
+        reader.close()
+    } else {
+        responseBody.append("Error en la petición. Código de respuesta: $responseCode")
+    }
+
+    connection.disconnect()
+
+    return responseBody.toString()
 }
--- kotlin/src/main/kotlin/Product.kt
+++ kotlin/src/main/kotlin/Product.kt
@@ -1,2 +1,18 @@
-class Product {
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+@Serializable
+data class Product ( val id: Long,
+                     val title: String,
+                     val description: String,
+                     val price: Long,
+                     val discountPercentage: Double,
+                     val rating: Double,
+                     val stock: Long,
+                     val brand: String,
+                     val category: String,
+                     val thumbnail: String,
+                     val images: List<String>){
+
+
+
 }
