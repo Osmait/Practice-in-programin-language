c65247627e7e9749ebbca3cbf61d709d38c9ce6f
osmait <saulburgos6@gmail.com>
2023-10-14 20:07:56 -0400

    format

--- C/algoritthm-book/printtree_r.c
+++ C/algoritthm-book/printtree_r.c
@@ -1,36 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node
-{
-    struct node *next;
-    struct node *child;
-    char *data;
+typedef struct node {
+  struct node *next;
+  struct node *child;
+  char *data;
 } node;
 
-void printtree_r(node *node, int depth)
-{
-    int i;
-    while (node)
-    {
-        if (node->child)
-        {
-            for (i = 0; depth * 3; i++)
-            {
-                printf(" ");
-            }
-            printf("{\n");
+void printtree_r(node *node, int depth) {
+  int i;
+  while (node) {
+    if (node->child) {
+      for (i = 0; depth * 3; i++) {
+        printf(" ");
+      }
+      printf("{\n");
 
-            for (i = 0; i < depth * 3; i++)
-            {
-                printf(" ");
-            }
-            printf("%s\n", node->data);
-        }
+      for (i = 0; i < depth * 3; i++) {
+        printf(" ");
+      }
+      printf("%s\n", node->data);
     }
+  }
 }
 
-void printtree(node *root)
-{
-    printtree_r(root, 0);
-}
+void printtree(node *root) { printtree_r(root, 0); }
