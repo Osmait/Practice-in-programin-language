99ef9f48c7fde4039506ab168186b8471a51da22
osmait <saulburgos6@gmail.com>
2023-11-03 12:03:47 -0400

    add desing patther: observer python

--- rust/src/leetcode/twoSum.rs
+++ rust/src/leetcode/twoSum.rs
@@ -5,6 +5,7 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Option<Vec<i32>> {
 
     for (i, v) in nums.iter().enumerate() {
         let diff = target - v;
+
         if map.contains_key(&diff) {
             let k = map.get(&diff).unwrap();
             return Some(vec![*k as i32, i as i32]);
